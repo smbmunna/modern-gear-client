3401a6e06fbc347261f764fa215eee51113ef283
smbmunna <smbmunna@gmail.com>
2024-10-09 00:51:15 +0600

    Router setup done

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is homepage</h2>            
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    },
+]);
